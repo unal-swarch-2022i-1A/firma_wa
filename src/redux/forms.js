e5f09d870a85a772f24a41260782ccd8110661d6
NikcQ <nikoo576@hotmail.com>
2022-05-16 22:06:51 -0500

    first commit

---
+++ src/redux/forms.js
@@ -0,0 +1,11 @@
+import * as actionTypes from './ActionTypes';
+
+export const InitialFeedback = {
+    firstname: '',
+    lastname: '',
+    telnum: '',
+    email: '',
+    agree: false,
+    contactType: 'Tel.',
+    message: ''
+};
